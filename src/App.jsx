d7f66ca3bf024477becf859f815643f8ffb03fff
Anoop S <sanoopanoop444@gmail.com>
2026-02-24 13:19:16 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Counter from "./Components/Counter";
 // import Todo from "./Components/Todo";
+import Counter from "./Components/Counter";
 
 const App = () => {
   return (
     <div>
-      {/* <Todo /> */}
+      {/* <Todo/>   */}
       <Counter/>
     </div>
   );
