14bc66ba1d5208baedbad6c3ce93606576cf7cd2
Anoop S <sanoopanoop444@gmail.com>
2026-02-24 13:12:34 +0530

    Edit complete

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 const Todo = () => {
   const [list,listNames]=useState(["demo1"]);
   const [name,inputNames]=useState("");
+  const [edit,editName]=useState(null);
+
 
   const addNames=()=>{
     if(name.trim()!==""){
@@ -17,16 +19,33 @@ const Todo = () => {
   listNames(del);
 };
 
+const Edit=(index)=>{
+    inputNames(list[index]);
+    editName(index);
+}
+
+const updateName = () => {
+  if (name.trim() !== "") {
+    const updated = [...list];
+    updated[edit] = name;
+    listNames(updated);
+    editName(null);
+    inputNames("");
+  }
+};
+
   return (
     <div>
       <h1>Todo List</h1>
       <input type="text" value={name} onChange={(e)=>inputNames(e.target.value)} />
       <button onClick={addNames}>Add</button>
+      <button onClick={updateName}>Update</button>
 
       <ul>
         {list.map((n,i)=>(<li key={i}>
           {n}
           <button onClick={()=>deletNames(i)}>dlt</button>
+          <button onClick={()=>Edit(i)}>Edit</button>
 
         </li>))}
       </ul>
