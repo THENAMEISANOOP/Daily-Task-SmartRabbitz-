4c286dfea2156cba44f5ebc10d67cbae3edadf44
Anoop S <sanoopanoop444@gmail.com>
2026-02-25 13:43:25 +0530

    fetching users using useeffect

--- src/Components/Learn_useEffect.jsx
+++ src/Components/Learn_useEffect.jsx
@@ -2,7 +2,28 @@ import React, { useEffect, useState } from 'react'
 
 const Learn_useEffect = () => {
 
-    const [count,setCount]=useState(0);
+    // const [count,setCount]=useState(0);
+    const[user,isUser]=useState([]);
+
+    
+
+    //fetching users
+      
+     useEffect(() => {
+    async function getUser() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+
+      const addlimlt= data.slice(0,3);
+      isUser(addlimlt)
+
+    //   isUser(data);
+    }
+
+    getUser();
+  }, []);
 
   //without dependency 
 
@@ -11,9 +32,9 @@ const Learn_useEffect = () => {
     // })
 
     //empty dependency -run only once (first render)
-    useEffect(()=>{
-        console.log("runnnig")
-    },[])
+    // useEffect(()=>{
+    //     console.log("runnnig")
+    // },[])
       
 //    with dependency  -run first render and count update time
     // useEffect(()=>{
@@ -23,8 +44,12 @@ const Learn_useEffect = () => {
 
   return (
     <div>
-        <p>Count:{count}</p>
-        <button onClick={()=>setCount(count+1)}>+</button>
+        <ul>
+            {user.map(u=>(<li key={u.id}>{u.name}</li>))}
+            
+        </ul>
+        {/* <p>Count:{count}</p>
+        <button onClick={()=>setCount(count+1)}>+</button> */}
     </div>
   )
 }
